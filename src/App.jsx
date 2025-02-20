fbd51ae1d1b0ea37a26432f626658e8c2a78126b
Abdalla Abdelmagid <abdallaa2024@gmail.com>
2025-02-19 19:35:14 -0500

    starting on the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import ProductList from './pages/ProductList'
-import ProductDetail from './pages/ProductDetail'
+
+import Profile from './pages/Profile'
 import './App.css'
 
 function App() {
@@ -14,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<ProductList />} />
           <Route path="/products/:id" element={<ProductDetail />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
       </main>
     </div>
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,10 @@
+function Profile() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      {/* We'll add more content here later */}
+    </div>
+  )
+}
+
+export default Profile 
