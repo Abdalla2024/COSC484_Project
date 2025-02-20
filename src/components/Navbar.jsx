35a64fe3860c04c27789a7119e31d4cd53271d12
Abdalla Abdelmagid <abdallaa2024@gmail.com>
2025-02-19 20:56:51 -0500

    Fixed black bars on sides and added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
   return (
-    <nav className="bg-white shadow-md p-4 fixed top-0 w-full z-50">
-      <div className="max-w-7xl mx-auto px-4 flex justify-between items-center">
+    <nav className="bg-white shadow-md p-2 fixed top-0 left-0 right-0 z-50 overflow-x-auto">
+      <div className="w-full flex justify-between items-center min-w-[1200px]">
         {/* Logo */}
-        <Link to="/" className="h-16">
+        <Link to="/" className="h-20 ml-16">
           <img 
             src="/towson-logo.png" 
             alt="Towson University Logo" 
@@ -13,9 +13,21 @@ function Navbar() {
           />
         </Link>
 
+        {/* Marketplace Title with Icon */}
+        <div className="flex items-center gap-4">
+          <img 
+            src="/market-icon.png"
+            alt="Market Icon"
+            className="h-12 w-12 object-contain"
+          />
+          <h1 className="text-5xl font-bold text-black">
+            Marketplace
+          </h1>
+        </div>
+
         {/* Profile Icon */}
-        <Link to="/profile" className="flex items-center mr-16">
-          <div className="w-12 h-12 overflow-hidden rounded-full border-2 border-gray-300">
+        <Link to="/profile" className="flex items-center mr-32">
+          <div className="w-14 h-14 overflow-hidden rounded-full border-2 border-gray-300">
             <img 
               src="/default-profile.png" 
               alt="Profile" 
