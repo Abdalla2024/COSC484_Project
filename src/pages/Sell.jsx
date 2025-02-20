7471257aaffed7852adf9295c5c1f4d7bbbc9bc9
Abdalla Abdelmagid <abdallaa2024@gmail.com>
2025-02-20 17:58:09 -0500

    added message and + button to navbar

---
+++ src/pages/Sell.jsx
@@ -0,0 +1,12 @@
+function Sell() {
+  return (
+    <div className="min-h-screen pt-20 px-4">
+      <div className="max-w-4xl mx-auto">
+        <h1 className="text-2xl font-bold text-gray-900 mb-6">Sell an Item</h1>
+        {/* Add your sell form content here */}
+      </div>
+    </div>
+  );
+}
+
+export default Sell; 
