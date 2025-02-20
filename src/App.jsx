94b19de97d470cf8bf3be8a88d69e07a96005c67
Abdalla Abdelmagid <abdallaa2024@gmail.com>
2025-02-19 20:16:45 -0500

    Added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-
+import Home from './pages/Home'
+import ProductList from './pages/ProductList'
+import ProductDetail from './pages/ProductDetail'
 import Profile from './pages/Profile'
 import './App.css'
 
 function App() {
   return (
-    <div className="app">
+    <div className="min-h-screen bg-white">
       <Navbar />
       <main className="container mx-auto px-4">
         <Routes>
