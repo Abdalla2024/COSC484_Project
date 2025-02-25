9a47d07072f352b8bbcb75e3927a6d6f254f3335
Abdalla Abdelmagid <abdallaa2024@gmail.com>
2025-02-25 18:44:02 -0500

    navbar now takes full width

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ function Navbar() {
 
   return (
     <nav className="fixed top-0 left-0 right-0 bg-white shadow-sm z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="w-full px-4">
         <div className="flex justify-between h-20 items-center">
           {/* Logo */}
           <div className="flex items-center">
