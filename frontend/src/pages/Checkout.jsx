e785b4060d9a2fe47a9e2a802113203fb43498b6
Dominick Winningham <dominickwinningham@gmail.com>
2025-05-09 12:14:27 -0400

    Commented out payment form box for now

--- frontend/src/pages/Checkout.jsx
+++ frontend/src/pages/Checkout.jsx
@@ -89,7 +89,7 @@ function Checkout() {
                     <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
                   </svg>
                   <p className="text-sm text-gray-500">
-                    {listing.deliveryMethod === "Shipping" || !listing.meetupLocation
+                    {listing.deliveryMethod === "Shipping" || !listing
                     ? "Delivery Only - no pickup location"
                     : `Meetup Location: ${listing.meetupLocation}`}
                   </p>
